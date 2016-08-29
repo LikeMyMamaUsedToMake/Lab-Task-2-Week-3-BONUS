985550a391f060c1bec8e04e9b1a986151111f66
LikeMyMamaUsedToMake <marco.nocera95@gmail.com>
2016-08-29 17:10:13 +1000

    TEST
    TEST

---
+++ Lab Task 2 Week 3 BONUS/Lab Task 2 Week 3 BONUS.cpp
@@ -0,0 +1,50 @@
+// Lab Task 2 Week 3 BONUS.cpp : Defines the entry point for the console application.
+//
+
+/*Perform three XOR over two numbers inserted by the user, then, attach the results to the 
+file inserted as parameter*/
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+
+{
+	int A, B;
+	/*Check whether two parameters have been inserted */
+	if (argc == 2){
+
+		ofstream Numbers(argv[1], std::ofstream::out | std::ofstream::app);
+		if (Numbers.is_open()){
+			cout << "Instert a number A:" << endl;
+			cin >> A;
+			cout << "Instert a number B:" << endl;
+			cin >> B;
+			A = A^B;
+			cout << A << endl;
+			Numbers << A << endl;
+			B = A^B;
+			cout << B << endl;
+			Numbers << B << endl;
+			A = A^B;
+			cout << A << endl;
+			Numbers << A << endl;
+
+			}
+			
+			Numbers.close();
+
+		}
+		else{
+			cout << "The file has not been opened correctly" << endl;
+		}
+
+
+
+	return 0;
+}
+
